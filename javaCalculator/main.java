958f629814ef5c0797545c394414ef7d440ff9a4
hyan <hyanvic9@gmail.com>
2026-05-03 22:14:14 -0400

    Corrigindo erros de sintaxe e lógica, e melhorando o loop

--- javaCalculator/main.java
+++ javaCalculator/main.java
@@ -13,6 +13,7 @@ public class main {
         int option;
 
         // 3. Entrada de dados do usuário.
+        // 3.1 While (true): cria um loop infinito.
         while (true) {
             System.out.println("\n--- MENU ---");
             System.out.println("1. Iniciar");
@@ -21,39 +22,55 @@ public class main {
 
             option = scanner.nextInt();
 
-        System.out.println("Digite um número: ");
-        num1 = scanner.nextDouble();
+            // 4. case das opçoes para o usuário escolher
+            switch (option) {
+                case 1:
+                    System.out.println("Seja bem vindo!");
+                    break;
+                case 2:
+                    System.out.println("Saindo do programa...");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }
 
-        System.out.println("Escolha uma operaçãp: +, -, *, ou /");
-        operation = scanner.next().charAt(0);
+            // 4.1 Lógica que finaliza o loop (do while) ao escolher a opção 2 (case 2).
+            if (option == 2) {
+                break;
+            }
 
-        System.out.println("Digite outro número: ");
-        num2 = scanner.nextDouble();
+            System.out.println("Digite um número: ");
+            num1 = scanner.nextDouble();
 
-        // 4. Lógica e funcionamento da calculadora.
-        switch (operation) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("Erro: divisão por zero!");
-                }
-                break;
-            default:
-                System.out.println("Operação inválida! escolha uma operação válida para prosseguir...");
-                break;
+            System.out.println("Escolha uma operaçãp: +, -, *, ou /");
+            operation = scanner.next().charAt(0);
+
+            System.out.println("Digite outro número: ");
+            num2 = scanner.nextDouble();
+
+            // 4. Lógica e funcionamento da calculadora.
+            switch (operation) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        System.out.println("Erro: divisão por zero!");
+                    }
+                    break;
+                default:
+                    System.out.println("Operação inválida! escolha uma operação válida para prosseguir...");
+            }
+            System.out.println("O resultado é: " + result);
         }
-        System.out.println("O resultado é: " + result);
         scanner.close();
-        }
     }
 }
