738c3d956560021a788ddf14e0473362a0e15539
hyan <hyanvic9@gmail.com>
2026-05-04 00:45:42 -0400

    Adicionando controle de fluxo no projeto, o usuário escolhe quando será exibido o menu e quando irá finalizar o programa.

--- javaCalculator/calculator.java
+++ javaCalculator/calculator.java
@@ -11,29 +11,32 @@ public class calculator {
         // 2. Variáveis do projeto
         int num1, num2, result = 0;
         char operation;
-        int option;
+        int option = 0;
         boolean hasError = false;
+        boolean hasMenu = true;
 
         // 3. Entrada de dados do usuário.
         // 3.1 While (true): cria um loop infinito.
-        while (true) {
-            System.out.println("\n--- MENU ---");
-            System.out.println("1. Iniciar");
-            System.out.println("2. Sair");
-            System.out.println("Escolha uma opção: ");
+        while (option != 2) {
+            if (hasMenu) {
+                System.out.println("\n--- MENU ---");
+                System.out.println("1. Iniciar");
+                System.out.println("2. Sair");
+                System.out.println("Escolha uma opção: ");
 
-            option = scanner.nextInt();
+                option = scanner.nextInt();
+            }
 
             // 4. case das opçoes para o usuário escolher
             switch (option) {
                 case 1:
-                    System.out.println("Seja bem vindo!");
+                    System.out.println("\n--- Seja bem vindo! ---");
                     break;
                 case 2:
-                    System.out.println("Saindo do programa...");
+                    System.out.println("\n--- Saindo do programa... ---");
                     break;
                 default:
-                    System.out.println("Opção inválida.");
+                    System.out.println("\n--- Opção inválida! ---");
                     break;
             }
 
@@ -46,51 +49,64 @@ public class calculator {
             }
 
             // 5. Inputs do usuário
-            try {
-                System.out.println("Digite um número: ");
-                num1 = scanner.nextInt();
+            if (option == 1) {
+                try {
+                    System.out.println("Digite um número: ");
+                    num1 = scanner.nextInt();
 
-                System.out.println("Escolha uma operaçãp: +, -, *, ou /");
-                operation = scanner.next().charAt(0);
+                    System.out.println("Escolha uma operaçãp: +, -, *, ou /");
+                    operation = scanner.next().charAt(0);
 
-                System.out.println("Digite outro número: ");
-                num2 = scanner.nextInt();
+                    System.out.println("Digite outro número: ");
+                    num2 = scanner.nextInt();
 
-                // 6. Lógica e funcionamento da calculadora.
-                switch (operation) {
-                    case '+':
-                        result = num1 + num2;
+                    // 6. Lógica e funcionamento da calculadora.
+                    switch (operation) {
+                        case '+':
+                            result = num1 + num2;
+                            break;
+                        case '-':
+                            result = num1 - num2;
+                            break;
+                        case '*':
+                            result = num1 * num2;
+                            break;
+                        case '/':
+                            result = num1 / num2;
+                            break;
+                        default:
+                            throw new IllegalArgumentException("Operação inválida!");
+                    }
+
+                    System.out.println("O resultado é: " + result);
+                    System.out.println("Operação finalizada!");
+                    System.out.println("Deseja: [1] Nova conta | [2] Voltar ao menu | [3] Sair");
+                    int decision = scanner.nextInt();
+
+                    if (decision == 1) {
+                        hasMenu = false;
+                    } else if (decision == 2) {
+                        hasMenu = true;
+                    } else {
                         break;
-                    case '-':
-                        result = num1 - num2;
-                        break;
-                    case '*':
-                        result = num1 * num2;
-                        break;
-                    case '/':
-                        result = num1 / num2;
-                        break;
-                    default:
-                        throw new IllegalArgumentException("Operação inválida!");
+                    }
+
+                } catch (ArithmeticException e) {
+                    hasError = true;
+                    System.out.println("Erro: Não é possível dividir por 0!");
+                } catch (InputMismatchException e) {
+                    hasError = true;
+                    System.out.println("Erro: Digite um número válido!");
+                } catch (IllegalArgumentException e) {
+                    hasError = true;
+                    System.out.println("Erro: " + e.getMessage());
                 }
-            } catch (ArithmeticException e) {
-                hasError = true;
-                System.out.println("Erro: Não é possível dividir por 0!");
-            } catch (InputMismatchException e) {
-                hasError = true;
-                System.out.println("Erro: Digite um número válido!");
-            } catch (IllegalArgumentException e) {
-                hasError = true;
-                System.out.println("Erro: " + e.getMessage());
-            }
 
-            if (hasError) {
-                break;
+                if (hasError) {
+                    hasMenu = true;
+                }
             }
-
-            System.out.println("O resultado é: " + result);
         }
-
         scanner.close();
     }
 }
